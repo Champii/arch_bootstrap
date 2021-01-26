8982e9f51dd14a053d163eab4858bac32589a82b
Champii <contact@champii.io>
2021-01-26 13:34:27 +0100

    baby steps

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,15 +22,15 @@ sudo -u $username makepkg -si
 
 
 sudo -u $username yay  -S yadm htop dhclient man-pages openvpn ntp nvm postgresql redis openssh cmake pavucontrol i3-wm \
-    vlc popcorntime cron raiseorlaunch picom libvterm zplug fd trash-cli ntfs-3g nautilus iotop jnettop \
+    vlc popcorntime cron raiseorlaunch picom libvterm  fd trash-cli ntfs-3g nautilus iotop jnettop \
     libreoffice virtualbox webtorrent-desktop deluge-gtk xdotool screen openvpn ntp nvm redis openssh
-    cmake pavucontrol vlc popcorntime cron raiseorlaunch picom libvterm zplug fd eaf trash-cli ntfs-3g nautilus dmenu terminus-font \
+    cmake pavucontrol dmenu terminus-font \
     tty-font-awesome-4 network-manager-applet NetworkManager brave xinput markdown \
     konsole-git exa bat fzf ripgrep loop navi how2 has fselect fasd browsh ddgr boom tldr tree scrot cod \
     emacs27-git valgrind rustup \
-    lightdm-webkit2-greeter lightdm-webkit-theme-aether lightdm-settings \
-    lxappearance \
-    libinput libinput-gestures xf86-input-libinput
+    lightdm lightdm-webkit2-greeter lightdm-webkit-theme-aether lightdm-settings \
+    lxappearance xorg-xinit make gcc alsa-utils feh perl-anyevent-i3 \
+    libinput libinput-gestures xorg-server starship
 
 
 sudo -u $username yadm clone https://github.com/Champii/dotfiles
